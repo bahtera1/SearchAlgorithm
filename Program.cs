dd703291b553666b8cf60ec4a75dc6a2c18836ca
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-10-28 16:34:39 +0700

    add function public void BinarySearch

--- Program.cs
+++ Program.cs
@@ -40,6 +40,46 @@ namespace SearchAlgorithm
                 arr[i] = Int32.Parse(s1);
             }
         }
+
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                //accept the number to be serached
+                Console.Write("\n Enter elemnt want you want to search: \n");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //apply binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //obtain the indeks of the elemnt in the array
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to serach for the elements of the array
+                while ((item != arr[mid]) & (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item > arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + " Found at position " + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " Not Found in the Array\n");
+                Console.WriteLine("\nNumber of comparison : " + ctr);
+
+                Console.Write("\nCantinue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
+
+            } while ((ch == 'y')||(ch == 'y'));
+        }
        
     }
 }
