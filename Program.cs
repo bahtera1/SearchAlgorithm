e4c9671974caa24083531647513e371e8d5ce045
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-10-28 16:07:24 +0700

    add function public void input

--- Program.cs
+++ Program.cs
@@ -15,6 +15,21 @@ namespace SearchAlgorithm
         //get the number of elemnts to store in the array
         int i; 
 
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("eneter the number of elements in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 elemnts \n");
+            }
+
+           
+        }
        
     }
 }
