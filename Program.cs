c02f3e37bb53ede63681024c3eb6572c9098a043
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-10-28 16:01:40 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class program
+    {
+        //array to be searched
+        int[] arr = new int[20];
+        //number of element in the array
+        int n;
+        //get the number of elemnts to store in the array
+        int i;
+
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
